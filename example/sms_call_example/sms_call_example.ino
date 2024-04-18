54d6d05f60f59fcef6e38bcbf71d9005bc30dd23
TDLOGY <93193876+TDLOGY@users.noreply.github.com>
2024-04-18 18:41:48 +0700

    Update sms_call_example.ino

--- example/sms_call_example/sms_call_example.ino
+++ example/sms_call_example/sms_call_example.ino
@@ -5,9 +5,10 @@
 #define MCU_SIM_BAUDRATE        115200
 #define MCU_SIM_TX_PIN              17
 #define MCU_SIM_RX_PIN              16
+#define MCU_SIM_EN_PIN              15
 
 // Please update number before test
-#define PHONE_NUMBER                "+84989....."
+#define PHONE_NUMBER                "+8498*****"
 
 void sim_at_wait()
 {
@@ -15,15 +16,19 @@ void sim_at_wait()
     while (simSerial.available()) {
         Serial.write(simSerial.read());
     }
+
+    Serial.write("Done\n");
 }
 
 bool sim_at_cmd(String cmd){
     simSerial.println(cmd);
     sim_at_wait();
+    return true;
 }
 
 bool sim_at_send(char c){
     simSerial.write(c);
+    return true;
 }
 
 void sent_sms()
@@ -54,13 +59,17 @@ void call()
 
 void setup() 
 {
-    delay(2000);
+    /*  Power enable  */
+    pinMode(MCU_SIM_EN_PIN,OUTPUT); 
+    digitalWrite(MCU_SIM_EN_PIN,LOW);
+
+    delay(20);
     Serial.begin(115200);
     Serial.println("\n\n\n\n-----------------------\nSystem started!!!!");
-    simSerial.begin(MCU_SIM_BAUDRATE, SERIAL_8N1, MCU_SIM_RX_PIN, MCU_SIM_TX_PIN);
 
-    // Delay 5s for power on
-    delay(5000);
+    // Delay 8s for power on
+    delay(8000);
+    simSerial.begin(MCU_SIM_BAUDRATE, SERIAL_8N1, MCU_SIM_RX_PIN, MCU_SIM_TX_PIN);
 
     // Check AT Command
     sim_at_cmd("AT");
@@ -79,6 +88,7 @@ void setup()
     pinMode(2,OUTPUT); 
     digitalWrite(2,HIGH);
 
+  delay(5000);   
     sent_sms();
 
     // Delay 5s
